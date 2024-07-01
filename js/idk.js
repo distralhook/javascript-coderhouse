33a2334fa34849ea3784aac69d6a37e24fd7a4e7
Leandro Hernandez <86750663+distralhook@users.noreply.github.com>
2024-07-01 20:51:01 -0300

    Tercer entregita
    por favor funciona!

---
+++ js/idk.js
@@ -0,0 +1,83 @@
+
+/*
+<form id="myForm">
+        <label for="inputField">Enter something:</label>
+        <input type="text" id="inputField" name="inputField">
+        <button type="submit">Submit</button>
+    </form>
+
+    <form class="best">
+        <label for="inputField">NUEVO RECORD! ESCRIBE TU NOMBRE:</label>
+        <input type="text" id="name">
+        <button type="submit">Ingresar nombre</button>
+    </form>
+*/
+/*
+document.getElementById("best").addEventListener("submit", function(event) {
+    // Get the value from the input field
+    let ganadora = document.getElementById("name").value;
+    // Store the value in local storage
+    localStorage.removeItem("mejorMovimientos")
+    localStorage.setItem("mejorMovimientos", ganadora);
+    
+    document.getElementById("best").style.display = none;
+});
+*/
+
+/*
+removeItem("mejorMovimientos", jugador);
+let bestJugador = new jugador(nom,moves,dificultad)
+*/
+//if termine el juego
+    /*
+    if(cartasRestantes==0){
+        
+        switch(difi){
+            case 4://FACIL
+                if(moves<=localStorage.getItem("mejorEasy", ganador.movimientos)){
+                    document.getElementsByClassName("best").style.display = flex;
+                    console.log("idkanymore");
+                }
+                break;*/
+/*            case 8://MEDIANO
+                if(moves<=localStorage.getItem("mejorMedi", ganador)){
+                    document.getElementsByClassName("best").style.display = flex;
+                }
+                break;
+            default://12 DIFICIL
+                if(moves<=localStorage.getItem("mejorHard", ganador)){
+                    document.getElementsByClassName("best").style.display = flex;
+                }
+                break;
+        }
+    }*/
+/*
+
+<!--
+    <form class="best">
+        <label for="inputField">NUEVO RECORD! ESCRIBE TU NOMBRE:</label>
+        <input type="text" id="name">
+        <button type="submit">Ingresar nombre</button>
+    </form>
+    -->
+
+*/
+
+
+/*
+
+class jugador{
+    constructor(nom,mov,d){
+        this.nombre=nom;
+        this.movimientos=mov;
+        this.dif=d;
+    }
+}
+let ganador = new jugador("Leandro",9999,4)
+localStorage.setItem("mejorEasy", ganador)
+//ganador.dif=8;
+//localStorage.setItem("mejorMedi", ganador)
+//ganador.dif=12;
+//localStorage.setItem("mejorHard", ganador)
+
+*/
